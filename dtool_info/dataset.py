ccab4089601799348f1621891ddc838ff711faed
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-09-12 13:09:23 +0100

    Implement 'dtool diff' command

--- dtool_info/dataset.py
+++ dtool_info/dataset.py
@@ -1,6 +1,14 @@
 """Commands for getting information about datasets."""
 
+import sys
 import click
+
+from dtoolcore import DataSet
+from dtoolcore.compare import (
+    diff_identifiers,
+    diff_sizes,
+    diff_content,
+)
 
 from dtool_cli.cli import (
     dataset_uri_argument,
@@ -26,3 +34,33 @@ def diff(dataset_uri, reference_dataset_uri):
     dataset are recalculated using the hashing algorithm of the reference
     dataset.
     """
+
+    def echo_diff(diff):
+        for d in diff:
+            line = "{}, {}, {}".format(d[0], d[1], d[2])
+            click.secho(line)
+
+    ds = DataSet.from_uri(dataset_uri)
+    ref_ds = DataSet.from_uri(reference_dataset_uri)
+
+    ids_diff = diff_identifiers(ds, ref_ds)
+    if len(ids_diff) > 0:
+        click.secho("Different identifiers", fg="red")
+        click.secho(
+            "ID, present in '{}', present in '{}'".format(ds.name, ref_ds.name))
+        echo_diff(ids_diff)
+        sys.exit(1)
+
+    sizes_diff = diff_sizes(ds, ref_ds)
+    if len(sizes_diff) > 0:
+        click.secho("Different sizes", fg="red")
+        click.secho("ID, size in '{}', size in '{}'".format(ds.name, ref_ds.name))
+        echo_diff(sizes_diff)
+        sys.exit(2)
+
+    content_diff = diff_content(ds, ref_ds)
+    if len(content_diff) > 0:
+        click.secho("Different content", fg="red")
+        click.secho("ID, hash in '{}', hash in '{}'".format(ds.name, ref_ds.name))
+        echo_diff(content_diff)
+        sys.exit(3)
--- tests/test_compare_command.py
+++ tests/test_compare_command.py
@@ -9,6 +9,7 @@ from . import SAMPLE_DATASETS
 he_dataset_uri = "file://" + os.path.join(SAMPLE_DATASETS, "he")
 she_dataset_uri = "file://" + os.path.join(SAMPLE_DATASETS, "she")
 cat_dataset_uri = "file://" + os.path.join(SAMPLE_DATASETS, "cat")
+lion_dataset_uri = "file://" + os.path.join(SAMPLE_DATASETS, "lion")
 
 def test_dataset_diff_functional():
 
@@ -18,3 +19,15 @@ def test_dataset_diff_functional():
 
     result = runner.invoke(diff, [he_dataset_uri, he_dataset_uri])
     assert result.exit_code == 0
+
+    result = runner.invoke(diff, [he_dataset_uri, she_dataset_uri])
+    assert result.exit_code == 1
+    assert result.output.startswith("Different identifiers")
+
+    result = runner.invoke(diff, [cat_dataset_uri, lion_dataset_uri])
+    assert result.exit_code == 2
+    assert result.output.startswith("Different sizes")
+
+    result = runner.invoke(diff, [cat_dataset_uri, she_dataset_uri])
+    assert result.exit_code == 3
+    assert result.output.startswith("Different content")
